048bbf447e202de090c6838cf0c693701f6c3aa5
bobby.prani@gmail.com <bobby.prani@gmail.com@81f0028f-6826-4683-6960-7041bff7aa30>
2011-04-29 22:27:57 +0000

    basic dependent microbenchmarks

--- basic/read.c
+++ basic/read.c
@@ -1,26 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int *src;
+long *src;
 
 int main(int argc, char* argv[])
 {
-  unsigned long size = 256 * 1024 * 1024 / sizeof(int); // 256 MB
-  src = (int *)malloc(sizeof(int) * size);
-  unsigned long i = 0, j;
-  volatile register int dest;
+  unsigned long size = 256 * 1024 * 1024 / sizeof(long); // 256 MB
+  src = (long *)malloc(sizeof(long) * size);
+  printf("size of long %d\n", sizeof(long));
+  unsigned long i = 0, j, k;
+  volatile long dest1, dest2, dest3, dest4, dest5, dest6, dest7, dest8;
+  int num_mem_ops = 8;
 
-  unsigned long num_iter = 10000000000 / 5;
+  unsigned long num_iter = 1000000000 / num_mem_ops;
+  int inc = 16 * num_mem_ops;
   for(j = 0; j < num_iter; j++)
   {
-    dest = src[i];
-    dest = src[i+16];
-    dest = src[i+32];
-    dest = src[i+48];
-    dest = src[i+64];
+    dest1 += src[i];
+    dest2 += src[i];
+    dest3 += src[i];
+    dest4 += src[i];
+    dest5 += src[i];
+    dest6 += src[i];
+    dest7 += src[i];
+    dest8 += src[i];
 
-    i=+80;
-    if (i+80 >= size)
+    i+=inc;
+    if (i+(inc - 16) >= size)
       i = 0;
   }
 
